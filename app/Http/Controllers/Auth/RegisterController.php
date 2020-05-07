5fddff4e691e8e7e35e333f5355892c290cfaaba
haerulasnawi <Haerul.asnawi.ha@gmail.com>
2020-05-07 22:23:20 +0800

    register redirect

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -126,4 +126,12 @@ class RegisterController extends Controller
             return view('pages.nakes.profile.index');
         }
     }
+    public function redirectPath()
+    {
+        if (method_exists($this, 'redirectTo')) {
+            return $this->redirectTo();
+        }
+
+        return property_exists($this, 'redirectTo') ? $this->redirectTo : '/dashboard';
+    }
 }
