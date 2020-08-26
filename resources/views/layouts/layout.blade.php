817287bebb281e22b87983b91c53e4a7f762e62b
haerulasnawi <Haerul.asnawi.ha@gmail.com>
2020-08-26 18:49:39 +0800

    asset final

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,22 +7,22 @@
     <meta id="token" name="csrf-token" content="{{ csrf_token() }}">
     <title>Document</title>
     <!-- General CSS Files -->
-    <link rel="stylesheet" href="assets/modules/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{asset('modules/bootstrap/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="assets/modules/fontawesome/css/all.min.css">
+    {{-- <link rel="stylesheet" href="assets/modules/bootstrap/css/bootstrap.min.css"> --}}
+    <link rel="stylesheet" href="{{asset('assets/modules/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="'{{asset('assets/modules/fontawesome/css/all.min.css')}}'">
 
     <!-- CSS Libraries -->
-    <link rel="stylesheet" href="assets/modules/bootstrap-daterangepicker/daterangepicker.css">
-    <link rel="stylesheet" href="assets/modules/datatables/datatables.min.css">
-    <link rel="stylesheet" href="assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="assets/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css">
-    <link rel="stylesheet" href="assets/modules/bootstrap-social/bootstrap-social.css">
-    <link rel="stylesheet" href="assets/modules/summernote/summernote-bs4.css">
+    <link rel="stylesheet" href="{{asset('assets/modules/bootstrap-daterangepicker/daterangepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/modules/datatables/datatables.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/modules/bootstrap-social/bootstrap-social.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/modules/summernote/summernote-bs4.css')}}">
 
     <!-- Template CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/components.css">
-    <link rel="stylesheet" href="assets/css/custom.css">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/components.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/custom.css')}}">
     @stack('stylesheet')
     <!-- Start GA -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
@@ -52,33 +52,33 @@
     
 </body>
 <!-- General JS Scripts -->
-<script src="assets/modules/jquery.min.js"></script>
+<script src="{{asset('assets/modules/jquery.min.js')}}"></script>
 {{-- <script src="https://code.jquery.com/jquery-3.3.1.js"></script> --}}
-<script src="assets/modules/popper.js"></script>
-<script src="assets/modules/tooltip.js"></script>
-<script src="assets/modules/bootstrap/js/bootstrap.min.js"></script>
-<script src="assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
-<script src="assets/modules/moment.min.js"></script>
-<script src="assets/js/stisla.js"></script>
+<script src="{{asset('assets/modules/popper.js')}}"></script>
+<script src="{{asset('assets/modules/tooltip.js')}}"></script>
+<script src="{{asset('assets/modules/bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/modules/nicescroll/jquery.nicescroll.min.js')}}"></script>
+<script src="{{asset('assets/modules/moment.min.js')}}"></script>
+<script src="{{asset('assets/js/stisla.js')}}"></script>
 
 <!-- JS Libraies -->
-<script src="assets/modules/datatables/datatables.min.js"></script>    
-<script src="assets/modules/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js"></script>
-<script src="assets/modules/datatables/Select-1.2.4/js/dataTables.select.min.js"></script>
-<script src="assets/modules/jquery-ui/jquery-ui.min.js"></script>
-<script src="assets/modules/summernote/summernote-bs4.js"></script>
-<script src="assets/modules/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="{{asset('assets/modules/datatables/datatables.min.js')}}"></script>    
+<script src="{{asset('assets/modules/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('assets/modules/datatables/Select-1.2.4/js/dataTables.select.min.js')}}"></script>
+<script src="{{asset('assets/modules/jquery-ui/jquery-ui.min.js')}}"></script>
+<script src="{{asset('assets/modules/summernote/summernote-bs4.js')}}"></script>
+<script src="{{asset('assets/modules/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
 @include('sweetalert::alert')
 
 
 <!-- Page Specific JS File -->
-<script src="assets/js/page/modules-datatables.js"></script>
-<script src="assets/js/page/bootstrap-modal.js"></script>
+<script src="{{asset('assets/js/page/modules-datatables.js')}}"></script>
+<script src="{{asset('assets/js/page/bootstrap-modal.js')}}"></script>
 
 <!-- Template JS File -->
-<script src="assets/js/scripts.js"></script>
-<script src="assets/js/custom.js"></script>
-<script src="js/app.js"></script>
+<script src="{{asset('assets/js/scripts.js')}}"></script>
+<script src="{{asset('assets/js/custom.js')}}"></script>
+<script src="{{asset('js/app.js')}}"></script>
 @stack('script_bottom')
 </html>
